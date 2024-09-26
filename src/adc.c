0be2ec12ffd2077538d5e15143c62f11d63d6a46
Andreas Rodahl <andreas.rodahl@revolve.no>
2024-09-26 13:27:31 +0200

    Impelemnted PWM

---
+++ src/adc.c
@@ -0,0 +1,9 @@
+#include "adc.h"
+
+void setHardWired()
+{
+    TCCR1A = (1 << WGM10) | (1 << WGM11) | (1 << COM1A0) | (1 << COM1A1);
+    OCR1A = 0x0000;
+
+    TCCR1B = (1 << WGM12) | (1 << WGM13);
+}
