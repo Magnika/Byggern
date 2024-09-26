9dfaf0690d24421eab23f74ab8ab537ecb2ba23e
Andreas Rodahl <andreas.rodahl@revolve.no>
2024-09-26 13:30:16 +0200

    Set COM1A1=0

--- src/adc.c
+++ src/adc.c
@@ -2,7 +2,7 @@
 
 void setHardWired()
 {
-    TCCR1A = (1 << WGM10) | (1 << WGM11) | (1 << COM1A0) | (1 << COM1A1);
+    TCCR1A = (1 << WGM10) | (1 << WGM11) | (1 << COM1A0) | (0 << COM1A1);
     OCR1A = 0x0000;
 
     TCCR1B = (1 << WGM12) | (1 << WGM13);
