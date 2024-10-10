91769adc2fc06694137592b5b0b5d478f9db4c4c
aasmundrunningen <aasmund.runningen@gmail.com>
2024-10-10 13:14:26 +0200

    Moved documentation to header

--- include/spi.h
+++ include/spi.h
@@ -3,8 +3,18 @@
 
 #include "avr.h"
 
+/**
+ * @brief Initializes SPI as master
+ * 
+ */
 void spi_init();
 
+/**
+ * @brief Transmits a single byte. Reads the response (MISO) from SPI data register.
+ * 
+ * @param byte 
+ * @return char 
+ */
 char spi_transmit_byte(char byte);
 
 #endif // SPI_H
