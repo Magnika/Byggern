579aa7847f85a3aa6c0080f09edd0643cf5356be
aasmundrunningen <aasmund.runningen@gmail.com>
2024-10-10 16:07:53 +0200

    Enable interrupts on INT2

--- include/interruptSetup.h
+++ include/interruptSetup.h
@@ -15,8 +15,11 @@ void configure_interrupts()
     // Set INT0 to trigger on rising edge
     MCUCR |= (1 << ISC01) | (1 << ISC00);
 
+    // Set INT2 to trigger on rising edge
+    EMCUCR |= (1 << ISC2);
+
     // Set INT1 and INT0 to enable external interrupt
-    GICR |= (1 << INT1) | (1 << INT0);
+    GICR |= (1 << INT2) | (1 << INT1) | (1 << INT0);
 
     // Global interrupt enable
     SREG |= (1 << SREG_I);
@@ -30,6 +33,12 @@ ISR(INT0_vect)
 ISR(INT1_vect)
 {
     // TODO: Implement
+}
+
+ISR(INT2_vect)
+{
+    // CAN CONTROLLER
+    // Find out which CAN interrupt triggered by reading CANINTF
 }
 
 #endif // INTERRUPTSETUP_H
